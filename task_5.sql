9a3fadb9086a0d7d31d671c8ea49a31278c215ad
chepkuruiprudence <chepkuruiprudence403@gmail.com>
2025-10-26 23:20:49 +0300

    insert a row to an existing table

---
+++ task_5.sql
@@ -0,0 +1,3 @@
+-- Insert a single record into the CUSTOMERS table
+INSERT INTO CUSTOMERS (customer_id, customer_name, email, address)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
