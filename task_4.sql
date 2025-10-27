3217806e29c333b08f2f30d48cf6e96cecea5450
chepkuruiprudence <chepkuruiprudence403@gmail.com>
2025-10-27 10:38:47 +0300

    Add query to display full Books table description using INFORMATION_SCHEMA

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,13 @@
--- Print full description of the 'books' table
-SHOW CREATE TABLE books;
+-- Print full description of the 'Books' table without using DESCRIBE or EXPLAIN
+USE alx_book_store;
+
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    COLUMN_DEFAULT, 
+    EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
