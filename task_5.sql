7faaabbe9f20c400b1c0ce487da2067330951bf9
chepkuruiprudence <chepkuruiprudence403@gmail.com>
2025-10-27 10:49:26 +0300

    Fix: correct table name and syntax for task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,5 @@
--- Insert a single record into the CUSTOMERS table
-INSERT INTO CUSTOMERS (customer_id, customer_name, email, address)
+USE alx_book_store;
+
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+
